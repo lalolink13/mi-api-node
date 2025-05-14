bc26dfbfec55f95b6f4aa29191ef494000d18130
lalolink13 <longinus13@gmail.com>
2025-05-13 18:55:20 -0600

    cambios

--- db.js
+++ db.js
@@ -1,11 +1,7 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+
+const pool = mysql.createPool(process.env.DB_URL);
 
 module.exports = pool;
